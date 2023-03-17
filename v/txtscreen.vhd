22fbcfd8f5b97341005e389c3cbbe585a77d988f
asreed666 <areed@cityplym.ac.uk>
2023-03-17 09:29:20 +0000

    Text scoring
    text display tuning to do

--- v/txtscreen.vhd
+++ v/txtscreen.vhd
@@ -63,18 +63,18 @@ begin
     wren      => nWr,
     q         => vData
     );
-  vAddr         <= std_logic_vector(to_unsigned(40*((vp-2)/24) + (hp/16), 12));
-  char_row_addr <= std_logic_vector(to_unsigned(((conv_integer(vdata(7 downto 0)) -32)*12 + ((vp-2)/2) mod 12), 11));
+  vAddr         <= std_logic_vector(to_unsigned(40*((vp)/24) + (hp/16), 12));
+  char_row_addr <= std_logic_vector(to_unsigned(((conv_integer(vdata(7 downto 0)) -32)*12 + ((vp)/2) mod 12), 11));
 
   process(pClk)
   begin
     shifter(7 downto 0) <= q_8x12(7 downto 0);
     if rising_edge(pClk) then
 			if doubled = '0' then
-				pix <= shifter(7- (((hp/2)-1) mod 8));
+				pix <= shifter(6- (((hp/2)-2) mod 8));
 				doubled <= '1';
 			else
-				pix <= shifter(7- (((hp/2)-1) mod 8));
+				pix <= shifter(6- (((hp/2)-2) mod 8));
 				doubled <= '0';
 			end if;
     end if;
